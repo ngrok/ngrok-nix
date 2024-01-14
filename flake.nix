78f88a9de64649ffbb87ca73651bf51f443e1bd6
Josh Robson Chase <joshrc@ngrok.com>
2024-01-14 14:47:15 -0500

    add hydra test

--- flake.nix
+++ flake.nix
@@ -10,19 +10,35 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      docs =
-        pkgs.callPackage ./gen-module-docs.nix {
-          modules = [ self.nixosModules.ngrok ];
-        };
+      docs = pkgs.callPackage ./gen-module-docs.nix {
+        modules = [ self.nixosModules.ngrok ];
+      };
     in
     {
-      packages.write-module-docs = pkgs.writeShellScriptBin "write-module-docs" ''
+      packages.write-nixos-docs = pkgs.writeShellScriptBin "write-nixos-docs" ''
         cp ${docs} "$1"
       '';
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.gnumake
         ];
+      };
+    }) // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = (p: builtins.elem (lib.getName p) [
+          "ngrok"
+        ]);
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      hydraJobs = self.packages.${system} // {
+        test-nixos-module = (import ./nixos-test.nix {
+          inherit pkgs;
+          ngrokModule = self.nixosModules.ngrok;
+        });
       };
     });
 }
---
+++ nixos-test.nix
@@ -0,0 +1,28 @@
+{ pkgs, ngrokModule }:
+pkgs.nixosTest {
+  name = "ngrok-module";
+  nodes.machine = { config, pkgs, ... }: {
+    imports = [
+      ngrokModule
+    ];
+
+    services.ngrok = {
+      enable = true;
+      authtoken = "foobarbaz";
+      tunnels = {
+        test = {
+          proto = "http";
+          addr = "1234";
+        };
+      };
+    };
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    system.stateVersion = "22.05";
+  };
+  testScript = { nodes, ... }: ''
+    machine.wait_for_unit("ngrok.service")
+  '';
+}
